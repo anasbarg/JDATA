24a9c6b833cd4b165fd865bd0e51af311f34e0f1
Anas Barg <anasbar97@gmail.com>
2018-10-19 12:17:33 -0700

    creating material navbar

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { MaterialModule } from "./material";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, NavbarComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ frontend/src/app/material.ts
@@ -0,0 +1,7 @@
+import { MatToolbarModule } from "@angular/material";
+import { NgModule } from "@angular/core";
+@NgModule({
+  imports: [MatToolbarModule],
+  exports: [MatToolbarModule]
+})
+export class MaterialModule {}
