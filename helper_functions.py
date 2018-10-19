c255815c72bdb66897319debbe49bc715ded99b1
Anas Barg <anasbar97@gmail.com>
2018-10-19 12:19:36 -0700

    adding labels attr to sliders of type yearSlider

--- helper_functions.py
+++ helper_functions.py
@@ -1,5 +1,6 @@
 import xml.etree.ElementTree as ET
 def xml_to_dict(xml_path):
+    default_yearSlider_labels = [1992, 2002, 2006, 2008, 2010, 2013]
     tree = ET.parse(xml_path)
     root = tree.getroot()
     dict_ = {
@@ -20,11 +21,25 @@ def xml_to_dict(xml_path):
         dict_["charts"].append(chart_dict)
 
     for chart in dict_["charts"]:
+
+        # converting "true" and "false" strings to Python boolean objects
         for dataset in chart["datasets"]:
             if dataset["fill"] == "true":
                 dataset["fill"] = True
             else:
                 dataset["fill"] = False
+        if chart["options"]["legendDisplay"] == "true":
+            chart["options"]["legendDisplay"] = True
+        else:
+            chart["options"]["legendDisplay"] = False
+        
+        # converting stringified lists "[...]" to Python list objects
+        for slider in chart["sliders"]:
+            if "labels" in slider and slider["type"] is "yearSlider":
+                slider["labels"] = eval(slider["labels"])
+            if "labels" not in slider and slider["type"] is "yearSlider":
+                slider["labels"] = default_yearSlider_labels
+
     return dict_
 
 
