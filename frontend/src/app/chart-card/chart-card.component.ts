1432e07c8975af2cd34ed6a47c037bb60966eaca
Anas Barg <anasbar97@gmail.com>
2018-10-22 04:41:04 -0700

    Adding Dataflow from content to chart-card

--- frontend/src/app/chart-card/chart-card.component.ts
+++ frontend/src/app/chart-card/chart-card.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from "@angular/core";
+import { Chart } from "../Types/Chart";
 @Component({
-  selector: 'app-chart-card',
-  templateUrl: './chart-card.component.html',
-  styleUrls: ['./chart-card.component.scss']
+  selector: "app-chart-card",
+  templateUrl: "./chart-card.component.html",
+  styleUrls: ["./chart-card.component.scss"]
 })
 export class ChartCardComponent implements OnInit {
+  @Input("chart")
+  chart: Chart;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
