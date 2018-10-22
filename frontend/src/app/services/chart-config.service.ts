3aeb3b6427f92fc8589e044b80eb00d67ad475e6
Anas Barg <anasbar97@gmail.com>
2018-10-22 02:55:31 -0700

    Using Hardcoded Config Instead Of Fetching It From The API

--- frontend/src/app/services/chart-config.service.ts
+++ frontend/src/app/services/chart-config.service.ts
@@ -1,9 +1,166 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable, of } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class ChartConfigService {
+  chartConfig: Object = JSON.parse(`{
+    "charts": [{
+        "data_provider": "Threshold_range",
+        "title": "Boundaries Of Average Income",
+        "subtitle": "Please Specify The Intervals",
+        "type": "line",
+        "fillBetween": "true",
+        "sliders": [{
+            "type": "yearSlider",
+            "labels": "[1992, 2002, 2006, 2008, 2010, 2013]"
+        }, {
+            "type": "percentageSlider"
+        }],
+        "datasets": [{
+            "dataset_id": "min",
+            "label": "A label",
+            "color": "rgb(90, 30, 255)",
+            "borderWidth": "1",
+            "fill": true
+        }, {
+            "dataset_id": "max",
+            "label": "A label",
+            "color": "rgb(90, 20, 255)",
+            "borderWidth": "1",
+            "fill": true
+        }],
+        "options": {
+            "pointRadius": "2",
+            "legendDisplay": true,
+            "yAxis": {
+                "min": "0",
+                "max": "1",
+                "scaleLabel": ""
+            },
+            "xAxis": {
+                "scaleLabel": ""
+            }
+        }
+    }, {
+        "data_provider": "Threshold_range",
+        "title": "Boundaries Of Average Income",
+        "subtitle": "Please Specify The Intervals",
+        "type": "line",
+        "fillBetween": "true",
+        "sliders": [{
+            "type": "yearSlider",
+            "labels": "[1992, 2002, 2006, 2008, 2010, 2013]"
+        }, {
+            "type": "percentageSlider"
+        }],
+        "datasets": [{
+            "dataset_id": "min",
+            "label": "A label",
+            "color": "rgb(90, 30, 255)",
+            "borderWidth": "1",
+            "fill": true
+        }, {
+            "dataset_id": "max",
+            "label": "A label",
+            "color": "rgb(90, 20, 255)",
+            "borderWidth": "1",
+            "fill": true
+        }],
+        "options": {
+            "pointRadius": "2",
+            "legendDisplay": true,
+            "yAxis": {
+                "min": "0",
+                "max": "1",
+                "scaleLabel": ""
+            },
+            "xAxis": {
+                "scaleLabel": ""
+            }
+        }
+    }, {
+        "data_provider": "Threshold_range",
+        "title": "Boundaries Of Average Income",
+        "subtitle": "Please Specify The Intervals",
+        "type": "line",
+        "fillBetween": "true",
+        "sliders": [{
+            "type": "yearSlider",
+            "labels": "[1992, 2002, 2006, 2008, 2010, 2013]"
+        }, {
+            "type": "percentageSlider"
+        }],
+        "datasets": [{
+            "dataset_id": "min",
+            "label": "A label",
+            "color": "rgb(90, 30, 255)",
+            "borderWidth": "1",
+            "fill": true
+        }, {
+            "dataset_id": "max",
+            "label": "A label",
+            "color": "rgb(90, 20, 255)",
+            "borderWidth": "1",
+            "fill": true
+        }],
+        "options": {
+            "pointRadius": "2",
+            "legendDisplay": true,
+            "yAxis": {
+                "min": "0",
+                "max": "1",
+                "scaleLabel": ""
+            },
+            "xAxis": {
+                "scaleLabel": ""
+            }
+        }
+    }, {
+        "data_provider": "Threshold_range",
+        "title": "Boundaries Of Average Income",
+        "subtitle": "Please Specify The Intervals",
+        "type": "line",
+        "fillBetween": "true",
+        "sliders": [{
+            "type": "yearSlider",
+            "labels": "[1992, 2002, 2006, 2008, 2010, 2013]"
+        }, {
+            "type": "percentageSlider"
+        }],
+        "datasets": [{
+            "dataset_id": "min",
+            "label": "A label",
+            "color": "rgb(90, 30, 255)",
+            "borderWidth": "1",
+            "fill": true
+        }, {
+            "dataset_id": "max",
+            "label": "A label",
+            "color": "rgb(90, 20, 255)",
+            "borderWidth": "1",
+            "fill": true
+        }],
+        "options": {
+            "pointRadius": "2",
+            "legendDisplay": true,
+            "yAxis": {
+                "min": "0",
+                "max": "1",
+                "scaleLabel": ""
+            },
+            "xAxis": {
+                "scaleLabel": ""
+            }
+        }
+    }]
+}`);
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  public getChartConfig(): Observable<Object> {
+    return of(this.chartConfig);
+  }
 }
