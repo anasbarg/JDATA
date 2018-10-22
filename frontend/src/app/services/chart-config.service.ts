834ad1fc78cb73266672c72617a8f53d48a82412
Anas Barg <anasbar97@gmail.com>
2018-10-22 04:12:22 -0700

    Using ChartConfig Type

--- frontend/src/app/services/chart-config.service.ts
+++ frontend/src/app/services/chart-config.service.ts
@@ -1,23 +1,24 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable, of } from "rxjs";
+import { ChartConfig } from "../Types/ChartConfig";
 
 @Injectable({
   providedIn: "root"
 })
 export class ChartConfigService {
-  chartConfig: Object = {
+  chartConfig: ChartConfig = {
     charts: [
       {
-        data_provider: "Threshold_range",
+        dataProvider: "Threshold_range",
         title: "Boundaries Of Average Income",
         subtitle: "Please Specify The Intervals",
         type: "line",
-        fillBetween: "true",
+        fillBetween: true,
         sliders: [
           {
             type: "yearSlider",
-            labels: "[1992, 2002, 2006, 2008, 2010, 2013]"
+            labels: [1992, 2002, 2006, 2008, 2010, 2013]
           },
           {
             type: "percentageSlider"
@@ -25,26 +26,26 @@ export class ChartConfigService {
         ],
         datasets: [
           {
-            dataset_id: "min",
+            datasetId: "min",
             label: "A label",
             color: "rgb(90, 30, 255)",
-            borderWidth: "1",
+            borderWidth: 1,
             fill: true
           },
           {
-            dataset_id: "max",
+            datasetId: "max",
             label: "A label",
             color: "rgb(90, 20, 255)",
-            borderWidth: "1",
+            borderWidth: 1,
             fill: true
           }
         ],
         options: {
-          pointRadius: "2",
+          pointRadius: 2,
           legendDisplay: true,
           yAxis: {
-            min: "0",
-            max: "1",
+            min: 0,
+            max: 1,
             scaleLabel: ""
           },
           xAxis: {
@@ -53,15 +54,15 @@ export class ChartConfigService {
         }
       },
       {
-        data_provider: "Threshold_range",
+        dataProvider: "Threshold_range",
         title: "Income Distribution",
         subtitle: "Please Select The Year",
         type: "line",
-        fillBetween: "true",
+        fillBetween: true,
         sliders: [
           {
             type: "yearSlider",
-            labels: "[1992, 2002, 2006, 2008, 2010, 2013]"
+            labels: [1992, 2002, 2006, 2008, 2010, 2013]
           },
           {
             type: "percentageSlider"
@@ -69,26 +70,26 @@ export class ChartConfigService {
         ],
         datasets: [
           {
-            dataset_id: "min",
+            datasetId: "min",
             label: "A label",
             color: "rgb(90, 30, 255)",
-            borderWidth: "1",
+            borderWidth: 1,
             fill: true
           },
           {
-            dataset_id: "max",
+            datasetId: "max",
             label: "A label",
             color: "rgb(90, 20, 255)",
-            borderWidth: "1",
+            borderWidth: 1,
             fill: true
           }
         ],
         options: {
-          pointRadius: "2",
+          pointRadius: 2,
           legendDisplay: true,
           yAxis: {
-            min: "0",
-            max: "1",
+            min: 0,
+            max: 1,
             scaleLabel: ""
           },
           xAxis: {
@@ -97,15 +98,15 @@ export class ChartConfigService {
         }
       },
       {
-        data_provider: "Threshold_range",
+        dataProvider: "Threshold_range",
         title: "Share",
         subtitle: "Please Specify The Intervals",
         type: "line",
-        fillBetween: "true",
+        fillBetween: true,
         sliders: [
           {
             type: "yearSlider",
-            labels: "[1992, 2002, 2006, 2008, 2010, 2013]"
+            labels: [1992, 2002, 2006, 2008, 2010, 2013]
           },
           {
             type: "percentageSlider"
@@ -113,26 +114,26 @@ export class ChartConfigService {
         ],
         datasets: [
           {
-            dataset_id: "min",
+            datasetId: "min",
             label: "A label",
             color: "rgb(90, 30, 255)",
-            borderWidth: "1",
+            borderWidth: 1,
             fill: true
           },
           {
-            dataset_id: "max",
+            datasetId: "max",
             label: "A label",
             color: "rgb(90, 20, 255)",
-            borderWidth: "1",
+            borderWidth: 1,
             fill: true
           }
         ],
         options: {
-          pointRadius: "2",
+          pointRadius: 2,
           legendDisplay: true,
           yAxis: {
-            min: "0",
-            max: "1",
+            min: 0,
+            max: 1,
             scaleLabel: ""
           },
           xAxis: {
