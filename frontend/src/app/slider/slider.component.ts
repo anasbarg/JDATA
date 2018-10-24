af65a77448840d036d016876d705ed6b54eff0b9
Anas Barg <anasbar97@gmail.com>
2018-10-24 08:02:29 -0700

    Creating dataflow from chart-controllers comp to slider comp

--- frontend/src/app/slider/slider.component.ts
+++ frontend/src/app/slider/slider.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
+import { Slider } from "../Types/Slider";
 
 @Component({
-  selector: 'app-slider',
-  templateUrl: './slider.component.html',
-  styleUrls: ['./slider.component.scss']
+  selector: "app-slider",
+  templateUrl: "./slider.component.html",
+  styleUrls: ["./slider.component.scss"]
 })
 export class SliderComponent implements OnInit {
+  @Input("slider-config")
+  sliderConfig: Slider;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
