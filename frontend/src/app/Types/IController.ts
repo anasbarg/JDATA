e8a5e8a10c65ffce458ea205f36a6442b2e70e09
Anas Barg <anasbar97@gmail.com>
2018-10-24 10:31:12 -0700

    Adding a very very important comment

--- frontend/src/app/Types/IController.ts
+++ frontend/src/app/Types/IController.ts
@@ -1,1 +1,14 @@
-export interface IController {}
+export interface IController {
+  /*
+    Every class that implements this should have
+    is{$className}() function where $className is the name
+    of the class that implements this interface .. Example:
+        class Slider implements IController {
+            isSlider() {
+                return true;
+            }
+        }
+    
+    As shown above, this function always returns true;
+    */
+}
