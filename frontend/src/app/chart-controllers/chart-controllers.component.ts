3613d10a81f18bfc2b232bbe042c47f926588747
Anas Barg <anasbar97@gmail.com>
2018-10-24 07:09:25 -0700

    Creating data flow from chart-card to chart-controllers

--- frontend/src/app/chart-controllers/chart-controllers.component.ts
+++ frontend/src/app/chart-controllers/chart-controllers.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from "@angular/core";
+import { Slider } from "../Types/Slider";
 @Component({
-  selector: 'app-chart-controllers',
-  templateUrl: './chart-controllers.component.html',
-  styleUrls: ['./chart-controllers.component.scss']
+  selector: "app-chart-controllers",
+  templateUrl: "./chart-controllers.component.html",
+  styleUrls: ["./chart-controllers.component.scss"]
 })
 export class ChartControllersComponent implements OnInit {
-
-  constructor() { }
+  @Input("chart")
+  chart: Array<Slider>;
+  constructor() {}
 
   ngOnInit() {
+    console.log(this.chart);
   }
-
 }
