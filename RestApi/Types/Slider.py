12095004dafd34f62df4cb4692cab73a1d18c80b
Anas Barg <anasbar97@gmail.com>
2018-10-25 12:10:14 -0700

    Adding title attribute to Slider type

--- RestApi/Types/Slider.py
+++ RestApi/Types/Slider.py
@@ -1,6 +1,6 @@
 class Slider:
     pips = None
-    label = ""
+    title = ""
     def __init__(self, **kwargs):
         # handling required kwargs
         try:
@@ -11,13 +11,13 @@ class Slider:
         # handling optional kwargs
         if "pips" in kwargs:
             self.pips : List[int] = kwargs["pips"]
-        if "label" in kwargs:
-            self.label : str = kwargs["label"]
+        if "title" in kwargs:
+            self.title : str = kwargs["title"]
     
     def to_dict(self):
         dict_ = {}
         dict_["pips"] = self.pips
-        dict_["label"] = self.label
+        dict_["title"] = self.title
         dict_["type"] = "slider"
         dict_["slider_type"] = self.type
         return dict_
--- frontend/src/app/Types/Slider.ts
+++ frontend/src/app/Types/Slider.ts
@@ -2,15 +2,23 @@ import { IController } from "./IController";
 
 export class Slider implements IController {
   type: string;
-  label: string;
+  title: string;
   pips: Array<any>;
-  constructor(type: string, label?: string, pips?: Array<any>) {
+  constructor(type: string, title?: string, pips?: Array<any>) {
     this.type = type;
-    this.label = label ? label : "";
+    this.title = title ? title : "";
     this.pips = pips ? pips : null;
   }
 
   isSlider() {
     return true;
+  }
+
+  isDropdown() {
+    return false;
+  }
+
+  isCheckbox() {
+    return false;
   }
 }
