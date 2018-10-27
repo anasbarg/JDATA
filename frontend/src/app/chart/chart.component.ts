9c74cf2c37fdf78c03c57ad1fbbf7b6740edb5d6
Anas Barg <anasbar97@gmail.com>
2018-10-27 08:21:22 +0200

    Passing chart config to the Chart component.

--- frontend/src/app/chart/chart.component.ts
+++ frontend/src/app/chart/chart.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Chart } from '../Types/Chart';
 
 @Component({
-  selector: 'app-chart',
-  templateUrl: './chart.component.html',
-  styleUrls: ['./chart.component.scss']
+    selector: 'app-chart',
+    templateUrl: './chart.component.html',
+    styleUrls: ['./chart.component.scss'],
 })
 export class ChartComponent implements OnInit {
+    @Input()
+    chart: Chart;
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit() {
-  }
-
+    ngOnInit() {}
 }
